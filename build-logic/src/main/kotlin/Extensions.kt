245596426e5cc0f73daa83c2517edefb2bc3d53b
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-06 17:09:54 -0700

    build: Update run-paper to 1.0.2

--- build-logic/src/main/kotlin/Extensions.kt
+++ build-logic/src/main/kotlin/Extensions.kt
@@ -1,11 +1,10 @@
 import net.kyori.indra.git.IndraGitExtension
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.the
 
 fun Project.lastCommitHash(): String =
-  extensions.getByType(IndraGitExtension::class).commit()?.name?.substring(0, 7) ?: "unknown"
+  the<IndraGitExtension>().commit()?.name?.substring(0, 7) ?: "unknown"
 
 val Project.libs: LibrariesForLibs
   get() = the()
