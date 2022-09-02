bf9d92150c008d2ff0d6aae7bf32179f638d27a2
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-09-01 18:50:49 -0700

    build: Update loom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
 
 plugins {
   id("ca.stellardrift.polyglot-version-catalogs") version "5.0.1"
-  id("quiet-fabric-loom") version "0.12-SNAPSHOT"
+  id("quiet-fabric-loom") version "1.0-SNAPSHOT"
 }
 
 rootProject.name = "MiniMOTD"
