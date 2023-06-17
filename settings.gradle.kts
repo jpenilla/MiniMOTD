add32b31e7095f3dfc96559b5cd782364baa3b0d
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-06-17 10:08:41 -0700

    fabric: 1.20.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
 
 plugins {
   id("ca.stellardrift.polyglot-version-catalogs") version "6.0.1"
-  id("quiet-fabric-loom") version "1.1-SNAPSHOT"
+  id("quiet-fabric-loom") version "1.2-SNAPSHOT"
 }
 
 rootProject.name = "MiniMOTD"
