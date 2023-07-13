2fd3d41cbd228ed89072be56822420c667fd4598
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-07-13 11:01:31 -0700

    build: Remove quilt repo
    Quiltflower is succeeded by Vineflower which is on Maven Central.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,6 @@ pluginManagement {
   repositories {
     gradlePluginPortal()
     maven("https://maven.fabricmc.net/")
-    maven("https://maven.quiltmc.org/repository/release/")
     maven("https://repo.jpenilla.xyz/snapshots/")
   }
   includeBuild("build-logic")
